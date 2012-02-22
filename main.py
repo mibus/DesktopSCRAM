a3ef0e07cce39dc2e6a843398f6677315abe19e7
Robert Mibus <mibus@mibus.org>
2012-02-22 15:50:04 +1030

    Make it work better in production

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ import serial
 import termios
 import time
 import os
+import sys
 
 while True:
 	try:
+		print "Trying to open a connection..."
 		# Open the serial device; this is the specific USB/Serial adapter model I'm using
 		s = serial.Serial('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0')
+		print "Got!"
 		while True:
+			print "Waiting for DTR change..."
 			fcntl.ioctl(s.fd, termios.TIOCMIWAIT, (termios.TIOCM_DSR))
-			dsr = s.getDSR()
-			print "Got a DSR interrupt, DSR is now " % (dsr)
 			# Do useful stuff
 			os.system('gnome-screensaver-command -l')
-			os.system('ps -C banshee && banshee --stop')
+			os.system('ps -C banshee > /dev/null && banshee --pause')
 			# Debounce
 			time.sleep(5)
 	except:
 		# Something died. Wait and try again.
+		print "Failed. Will try again in a bit."
+		print sys.exc_info()[0]
 		time.sleep(30)
 		pass
