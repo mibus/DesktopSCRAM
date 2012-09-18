80179c7acb355c4a29d277240de56affe604d25d
Robert Mibus <rmibus@internode.com.au>
2012-09-18 14:24:08 +0930

    Deal with stuck devices better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import fcntl
 import serial
+import signal
 import termios
 import time
 import os
@@ -22,28 +23,42 @@ else:
 	print 'Unable to determine OS (Linux or Darwin). Aborting.'
 	sys.exit(1)
 
-last_event = time.time()
+# Signal handler, just to ignore the SIGALRM we might get.
+def handler(signum, frame):
+	pass
+
+signal.signal(signal.SIGALRM, handler)
 
 # Main
 while True:
 	try:
 		print "Trying to open a connection..."
 		# Open the serial device; this is the specific USB/Serial adapter model I'm using
+		last_event = time.time()
 		s = serial.Serial('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0')
 		print "Got!"
 		while True:
-			print "Waiting for DTR change..."
-			fcntl.ioctl(s.fd, termios.TIOCMIWAIT, (termios.TIOCM_DSR))
-			# Debounce.
-			# (Also ignores events in the first 2s of runtime)
-			if time.time() - last_event < 2:
-				print "DTR changed, but ignored as a bounce"
-				continue
-			# Do useful stuff
-			print "DTR changed, activating!"
-			os.system(cmd_pause_music)
-			os.system(cmd_start_screensaver)
-			last_event = time.time();
+			# Check that the fd is still valid
+			if not s.isOpen(): break
+			# Set up a timeout, in case the device disappears for some reason
+			try:
+				# If we've been sitting & doing nothing for 15min, timeout in case the port has died in the meantime.
+				signal.alarm(900)
+				print "Waiting for DTR change..."
+				fcntl.ioctl(s.fd, termios.TIOCMIWAIT, (termios.TIOCM_DSR))
+				# Debounce.
+				# (Also ignores events in the first 2s of runtime)
+				if time.time() - last_event < 2:
+					print "DTR changed, but ignored as a bounce"
+					continue
+				# Do useful stuff
+				print "DTR changed, activating!"
+				os.system(cmd_pause_music)
+				os.system(cmd_start_screensaver)
+				last_event = time.time();
+			except IOError as e:
+				if e.errno != 4: raise e
+				pass
 	except:
 		# Something died. Wait and try again.
 		print "Failed. Will try again in a bit."
